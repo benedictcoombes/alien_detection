e1e1d3371aa85327ff95f225b634367f71c69c53
benedictcoombes <60744055+benedictcoombes@users.noreply.github.com>
2021-10-07 14:19:35 +0100

    Add files via upload

--- cov_deviation.py
+++ cov_deviation.py
@@ -1,33 +1,32 @@
 import sys
 
-prefix=sys.argv[2]
-window_size=sys.argv[3]
+window_size=sys.argv[4]
+prefix=sys.argv[5]
 
-with open('/ei/projects/c/c38b7c13-1c82-4b59-ae07-b631556928a3/scratch/introgression_work/introgression_detection_25_lines/introgression_lines/muticum_lines/parent_coverage/paragon/paragon_cov_norm_' + window_size + '.tsv', 'r') as paragon_cov_file:
-	with open('/ei/projects/c/c38b7c13-1c82-4b59-ae07-b631556928a3/scratch/introgression_work/introgression_detection_25_lines/introgression_lines/muticum_lines/parent_coverage/pavon/pavon_cov_norm_' + window_size + '.tsv', 'r') as pavon_cov_file:
-	# with open('/ei/projects/c/c38b7c13-1c82-4b59-ae07-b631556928a3/scratch/introgression_work/introgression_detection_25_lines/introgression_lines/muticum_lines/parent_coverage/CS/CS_cov_norm_' + window_size + '.tsv', 'r') as pavon_cov_file:
-		with open(sys.argv[1], 'r') as int_line_cov_file:
-			paragon = [[line.split( )[0], line.split( )[1], line.split( )[4]] for line in paragon_cov_file]
-			pavon = [[line.split( )[0], line.split( )[1], line.split( )[4]] for line in pavon_cov_file]
+with open(sys.argv[1], 'r') as int_line_cov_file:
+	with open(sys.argv[2], 'r') as wheat_parent_1_cov_file:
+		with open(sys.argv[3], 'r') as wheat_parent_2_cov_file:
+			wheat_parent_1 = [[line.split( )[0], line.split( )[1], line.split( )[4]] for line in wheat_parent_1_cov_file]
+			wheat_parent_2 = [[line.split( )[0], line.split( )[1], line.split( )[4]] for line in wheat_parent_2_cov_file]
 			int_line = [[line.split( )[0], line.split( )[1], line.split( )[3]] for line in int_line_cov_file]
 
 
 with open(prefix + "_cov_dev_" + window_size + ".tsv", 'w') as out_file:
 	for n, i in enumerate(int_line):
 		cov = float(i[2]) * 100
-		paragon_cov = float(paragon[n][2])
-		pavon_cov = float(pavon[n][2])
-		if paragon_cov == 0:
-			vs_paragon = 0
+		wheat_parent_1_cov = float(wheat_parent_1[n][2])
+		wheat_parent_2_cov = float(wheat_parent_2[n][2])
+		if wheat_parent_1_cov == 0:
+			vs_wheat_parent_1 = 0
 		else:
-			vs_paragon = cov / paragon_cov
-		if pavon_cov == 0:
-			vs_pavon = 0
+			vs_wheat_parent_1 = cov / wheat_parent_1_cov
+		if wheat_parent_2_cov == 0:
+			vs_wheat_parent_2 = 0
 		else:
-			vs_pavon = cov / pavon_cov
+			vs_wheat_parent_2 = cov / wheat_parent_2_cov
 
-		if abs(1-vs_paragon) <= abs(1-vs_pavon):
-			out_file.write("\t".join([str(i[0]), str(i[1]), str(vs_paragon)]) + "\n")
+		if abs(1-vs_wheat_parent_1) <= abs(1-vs_wheat_parent_2):
+			out_file.write("\t".join([str(i[0]), str(i[1]), str(vs_wheat_parent_1)]) + "\n")
 
 		else:
-			out_file.write("\t".join([str(i[0]), str(i[1]), str(vs_pavon)]) + "\n")
+			out_file.write("\t".join([str(i[0]), str(i[1]), str(vs_wheat_parent_2)]) + "\n")
